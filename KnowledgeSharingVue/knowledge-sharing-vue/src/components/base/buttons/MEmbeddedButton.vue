1bcea85368ddfb914e09baa06d4a68669fc81e11
Trần Văn Phúc <tranphuc8a@gmail.com>
2024-04-08 12:37:26 +0700

    Add EmbeddedButton

---
+++ KnowledgeSharingVue/knowledge-sharing-vue/src/components/base/buttons/MEmbeddedButton.vue
@@ -0,0 +1,41 @@
+<template>
+    <div @:click="resolveOnclick" class="p-button p-embedded-button" :state="data.state">
+        <div class="p-button-content">
+            <MIcon :fa="fa" v-if="fa != null" />
+            <div v-if="label != null"> {{ label }} </div>
+        </div>
+        <div class="p-loading-container">
+            <MSpinner/>
+        </div>
+    </div>
+</template>
+
+<script>
+import buttonScript from '@/js/components/base/button';
+let button = {
+    name: "EmbeddedButton",
+    data() {
+        return {
+            data: {
+                state: this.state,
+            }
+        };
+    },
+    methods: {
+        ...buttonScript.methods
+    },
+    watch: {
+        ...buttonScript.watch
+    },
+    props: {
+        ...buttonScript.props
+    },
+};
+export default button;
+</script>
+
+<style scoped>
+    @import url(@/css/base/button.css);
+</style>
+
+
